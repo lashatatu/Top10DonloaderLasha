96c4ef1f9824af1873b7e5be4e3960d762597099
lashatatu <lashatatu@gmail.com>
2020-06-10 15:06:46 +0400

    top10downloaderLasha
    update 0.03

---
+++ app/src/main/java/lasha/learntocode/top10donloaderlasha/ParseApplications.java
@@ -0,0 +1,16 @@
+package lasha.learntocode.top10donloaderlasha;
+
+import java.util.ArrayList;
+
+public class ParseApplications {
+    private static final String TAG = "ParseApplications";
+    private ArrayList<FeedEntry> applications;
+
+    public ParseApplications() {
+        this.applications = new ArrayList<>();
+    }
+
+    public ArrayList<FeedEntry> getApplications() {
+        return applications;
+    }
+}
